182740f69a61ff18b6cbd60ca0e6bcf9565be8d0
unknown <sebasguesa@hotmail.com>
2025-04-11 14:53:23 -0500

    Se agrega un codigo para hacer ping a una url en libreria scapy de python

---
+++ Codigo_Ciberseguridad/Escaneos/11_Scapy_PING_Url.py
@@ -0,0 +1,17 @@
+from scapy.all import IP, ICMP, sr1
+
+def ping(host):
+    # Se guarda en una variable la respuesta, de un icmp a un host o url
+    packet = IP(dst=host)/ICMP()
+    # Se guarda en una variable la repuesta de un sr1 con sus variables asignadas
+    response = sr1(packet, timeout=2, verbase=0)
+    # Si dio respuesta el ping entonces da una respuesta y si no, da otra
+    if response:
+        return f"{host} Esta en linea"
+    else:
+        return f"{host} No esta en linea"
+
+url = "www.google.com"
+result = ping(url)
+print(result)
+
